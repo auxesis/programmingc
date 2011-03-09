95a30bfffe175423153f1ae61b8a555b01c3155c
Jordan Liling <jliling@viola.it.uts.edu.au>
2011-03-09 19:54:39 +1100

    Initial commit. Added examples 1 + 2

---
+++ example2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+        int n1, n2, sum;
+
+        printf("Enter first integer: ");
+        scanf("%d", &n1);
+
+        printf("Enter second integer: ");
+        scanf("%d", &n2);
+
+        sum = n1 + n2; 
+        printf("Sum is %d\n", sum);
+
+        return 0;
+}
